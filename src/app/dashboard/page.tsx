97c9156368f4ca42fee897b7b1004168c01c4373
vivek-78 <akepoguvivek14@gmail.com>
2023-12-15 00:44:57 +0530

    added address form

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,9 +1,10 @@
 import { UserButton } from "@clerk/nextjs";
- 
+import AddressForm from "./AddressForm";
 export default function Home() {
   return (
     <div className="h-screen">
       <UserButton afterSignOutUrl="/"/>
+      <AddressForm />
     </div>
   )
 }
